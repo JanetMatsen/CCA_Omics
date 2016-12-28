18d3eb111cf2663ecc68fc4a219be7e06695f6a7
Janet Matsen <janetmatsen@gmail.com>
2016-12-27 17:12:18 -0800

    Found bug in PMA package; can now set `standardize = FALSE` in CCA
    This diff demonstrates that conversion of a Python-standardized
    data set from a data.frame instance to a matrix instance
    allows PMA's CCA method to work.
    
    The PMA package should convert data.frame instances to matrices
    or at least require you to provide a matrix.

--- dev/avoid_standardize_in_R/CCA_sklearn_data.R
+++ dev/avoid_standardize_in_R/CCA_sklearn_data.R
@@ -26,3 +26,30 @@ cca = CCA(x = x, z = z, K = 1,
           #standardize = TRUE
 )
 
+ss_version <- function(v){
+        sd <- apply(v, 2, sd)
+        if (min(sd) == 0)
+                stop("Cannot standardize because some of the columns of x have std. dev. 0")
+        return(scale(v, TRUE, sd))
+}
+
+x_ss_R <- ss_version(x)
+
+class(x) # data.frame
+class(x_ss_R)  # matrix
+
+x[0:4, 0:4]
+x_ss_R[0:4, 0:4]
+
+# Convert the data.frame to matrix and the SVD error disappears.
+x_matrix <- data.matrix(x)
+z_matrix <- data.matrix(z)
+
+# now this works!
+cca = CCA(x = x_matrix, z = z_matrix, K = 1,
+          typex = 'standard', typez = 'standard',
+          standardize = FALSE)
+
+# Conclusion:  there is a bug in the PMA package.
+# It should convert data.frame instances to matrices or at least require you to provide a matrix.
+
